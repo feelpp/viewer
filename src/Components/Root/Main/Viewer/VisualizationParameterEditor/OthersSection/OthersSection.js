303911e5452ac9e473045ece4062536ccc7dcae0
Félix Veysseyre <felix.veysseyre@gmail.com>
2018-11-21 16:59:24 +0100

    ENH: Implement OthersSection Component

---
+++ src/Components/Root/Main/Viewer/VisualizationParameterEditor/OthersSection/OthersSection.js
@@ -0,0 +1,157 @@
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import {formatExtensions, formatMIMETypes} from '../../../../../../Others/format.js';
+
+import {downloadImageURL} from '../../../../../../Helpers/download.js';
+
+import visualizationParametersActions from '../../../../../../Actions/visualizationParameters/visualizationParameters.js';
+
+import Button from '../../../../../Helpers/FormElements/Button/Button.js';
+import PanelSection from '../../../../../Helpers/Panel/PanelSection/PanelSection.js';
+import ValidationInput from '../../../../../Helpers/FormElements/ValidationInput/ValidationInput.js';
+
+import './OthersSection.less';
+
+export class OthersSection extends Component {
+
+	/* Generic */
+
+	constructor(props) {
+		super(props);
+
+		/* Attributes */
+	}
+
+	render() {
+
+		/* Element */
+
+		const element = (
+			<div
+				className="OthersSection"
+			>
+				<PanelSection
+					label="Others"
+					initialOpenStatus={this.props.initialOpenStatus}
+				>
+					<table
+						className="fields"
+					>
+						<tbody>
+							<tr
+								className="fieldLine"
+							>
+								<td
+									className="fieldLabel"
+								>
+									Background
+								</td>
+								<td
+									className="fieldEditor"
+								>
+									<ValidationInput
+										type="text"
+										value={this.props.backgroundColor}
+										checker={(backgroundColor) => {
+											return /[A-Fa-f0-9]{6}/.test(backgroundColor)
+										}}
+										action={(backgroundColor) => {
+											this.setBackgroundColor(backgroundColor);
+										}}
+									/>
+								</td>
+							</tr>
+							<tr
+								className="fieldLine"
+							>
+								<td
+									className="fieldLabel"
+								>
+									Screenshot
+								</td>
+								<td
+									className="fieldEditor"
+								>
+									<Button
+										disabled={this.props.screenShotGenerator === null}
+										action={() => {
+											this.downloadScreenShot();
+										}}
+									>
+										Download
+									</Button>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</PanelSection>
+			</div>
+		);
+
+		/* Return */
+
+		return element;
+	}
+
+	/* Specific */
+
+	setBackgroundColor(backgroundColor) {
+		this.props.client.Viewer.setBackgroundColor(backgroundColor).then((result) => {
+			if(result.value)
+			{
+				this.props.setBackgroundColor(backgroundColor);
+			}
+		});
+	}
+
+	downloadScreenShot() {
+		const format = this.props.configuration.screenShot.format;
+		const quality = this.props.configuration.screenShot.quality;
+
+		const MIMEType = formatMIMETypes[format];
+		const extension = formatExtensions[format];
+
+		const screenShotURL = this.props.screenShotGenerator(MIMEType, quality);
+
+		if(screenShotURL)
+		{
+			downloadImageURL(screenShotURL, 'screenshot.' + extension, MIMEType);
+		}
+	}
+}
+
+OthersSection.propTypes = {
+	initialOpenStatus: PropTypes.bool,
+	screenShotGenerator: PropTypes.func,
+
+	configuration: PropTypes.object.isRequired,
+	client: PropTypes.object.isRequired,
+	backgroundColor: PropTypes.string.isRequired,
+	setBackgroundColor: PropTypes.func.isRequired,
+};
+
+OthersSection.defaultProps = {
+	initialOpenStatus: null,
+	screenShotGenerator: null,
+};
+
+export default connect(
+	(state) => {
+		return {
+			initialOpenStatus: state.configuration.visualizationParameterEditor.sectionInitialOpenStatus.filters,
+			configuration: state.configuration,
+			client: state.connection.client,
+			backgroundColor: state.visualizationParameters.backgroundColor,
+			screenShotGenerator: state.screenShotGenerator,
+		};
+	},
+	(dispatch) => {
+		return {
+			setBackgroundColor: (backgroundColor) => {
+				dispatch(visualizationParametersActions.setBackgroundColor(backgroundColor));
+			},
+		};
+	}
+)(OthersSection);
--- src/Components/Root/Main/Viewer/VisualizationParameterEditor/VisualizationParameterEditor.js
+++ src/Components/Root/Main/Viewer/VisualizationParameterEditor/VisualizationParameterEditor.js
@@ -2,12 +2,7 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-import {formatExtensions, formatMIMETypes} from '../../../../../Others/format.js';
-
-import {downloadImageURL} from '../../../../../Helpers/download.js';
-
 import panelActions from '../../../../../Actions/panel/panel.js';
-import visualizationParametersActions from '../../../../../Actions/visualizationParameters/visualizationParameters.js';
 
 import ViewSection from './ViewSection/ViewSection.js';
 import ColorMapSection from './ColorMapSection/ColorMapSection.js';
@@ -15,10 +10,8 @@ import CameraSection from './CameraSection/CameraSection.js';
 import LegendSection from './LegendSection/LegendSection.js';
 import GridSection from './GridSection/GridSection.js';
 import FiltersSection from './FiltersSection/FiltersSection.js';
-import Button from '../../../../Helpers/FormElements/Button/Button.js';
+import OthersSection from './OthersSection/OthersSection.js';
 import Panel from '../../../../Helpers/Panel/Panel.js';
-import PanelSection from '../../../../Helpers/Panel/PanelSection/PanelSection.js';
-import ValidationInput from '../../../../Helpers/FormElements/ValidationInput/ValidationInput.js';
 
 import './VisualizationParameterEditor.less';
 
@@ -75,61 +68,7 @@ export class VisualizationParameterEditor extends Component {
 		/** Others **/
 
 		const othersPanelSection = (
-			<PanelSection
-				label="Others"
-				initialOpenStatus={this.props.configuration.visualizationParameterEditor.sectionInitialOpenStatus.others}
-			>
-				<table
-					className="fields"
-				>
-					<tbody>
-						<tr
-							className="fieldLine"
-						>
-							<td
-								className="fieldLabel"
-							>
-								Background
-							</td>
-							<td
-								className="fieldEditor"
-							>
-								<ValidationInput
-									type="text"
-									value={this.props.backgroundColor}
-									checker={(backgroundColor) => {
-										return /[A-Fa-f0-9]{6}/.test(backgroundColor)
-									}}
-									action={(backgroundColor) => {
-										this.setBackgroundColor(backgroundColor);
-									}}
-								/>
-							</td>
-						</tr>
-						<tr
-							className="fieldLine"
-						>
-							<td
-								className="fieldLabel"
-							>
-								Screenshot
-							</td>
-							<td
-								className="fieldEditor"
-							>
-								<Button
-									disabled={this.props.screenShotGenerator === null}
-									action={() => {
-										this.downloadScreenShot();
-									}}
-								>
-									Download
-								</Button>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</PanelSection>
+			<OthersSection/>
 		);
 
 		/* Element */
@@ -160,40 +99,11 @@ export class VisualizationParameterEditor extends Component {
 
 	/* Specific */
 
-	setBackgroundColor(backgroundColor) {
-		this.props.client.Viewer.setBackgroundColor(backgroundColor).then((result) => {
-			if(result.value)
-			{
-				this.props.setBackgroundColor(backgroundColor);
-			}
-		});
-	}
-
-	downloadScreenShot() {
-		const format = this.props.configuration.screenShot.format;
-		const quality = this.props.configuration.screenShot.quality;
-
-		const MIMEType = formatMIMETypes[format];
-		const extension = formatExtensions[format];
-
-		const screenShotURL = this.props.screenShotGenerator(MIMEType, quality);
-
-		if(screenShotURL)
-		{
-			downloadImageURL(screenShotURL, 'screenshot.' + extension, MIMEType);
-		}
-	}
 }
 
 VisualizationParameterEditor.propTypes = {
-	screenShotGenerator: PropTypes.func,
-
-	configuration: PropTypes.object.isRequired,
-	client: PropTypes.object.isRequired,
 	openStatus: PropTypes.bool.isRequired,
 	setOpenStatus: PropTypes.func.isRequired,
-	backgroundColor: PropTypes.string.isRequired,
-	setBackgroundColor: PropTypes.func.isRequired,
 };
 
 VisualizationParameterEditor.defaultProps = {
@@ -203,20 +113,13 @@ VisualizationParameterEditor.defaultProps = {
 export default connect(
 	(state) => {
 		return {
-			configuration: state.configuration,
-			client: state.connection.client,
 			openStatus: state.panel.openStatus,
-			backgroundColor: state.visualizationParameters.backgroundColor,
-			screenShotGenerator: state.screenShotGenerator,
 		};
 	},
 	(dispatch) => {
 		return {
 			setOpenStatus: (openStatus) => {
 				dispatch(panelActions.setOpenStatus(openStatus));
-			},
-			setBackgroundColor: (backgroundColor) => {
-				dispatch(visualizationParametersActions.setBackgroundColor(backgroundColor));
 			},
 		};
 	}
