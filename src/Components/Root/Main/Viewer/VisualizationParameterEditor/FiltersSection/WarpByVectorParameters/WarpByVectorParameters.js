31df09c89d3b92ba284de262234f97d6c93188e2
Félix Veysseyre <felix.veysseyre@gmail.com>
2018-11-22 15:53:04 +0100

    ENH: Add checker to warpByVector fitler scaleFactor input

--- src/Components/Root/Main/Viewer/VisualizationParameterEditor/FiltersSection/WarpByVectorParameters/WarpByVectorParameters.js
+++ src/Components/Root/Main/Viewer/VisualizationParameterEditor/FiltersSection/WarpByVectorParameters/WarpByVectorParameters.js
@@ -69,6 +69,9 @@ export class WarpByVectorParameters extends Component {
 						<ValidationInput
 							type="number"
 							value={this.props.scaleFactor}
+							checker={(scaleFactor) => {
+								return scaleFactor !== 0;
+							}}
 							action={(scaleFactor) => {
 								this.setScaleFactor(scaleFactor);
 							}}
